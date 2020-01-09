d29a29e2f0a2acc1b781d6b8d60d23cfdf97483e
Mark Vicuña <markvicuna@Marks-iMac.local>
2020-01-09 08:35:17 +0200

    Add exercises 3.12 to 3.18

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+mongoose.set('useUnifiedTopology', true);
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+    `mongodb+srv://mark:${password}@cluster0-gpszd.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+    person.save().then(response => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+} else {
+    console.log("phonebook:")
+    Person
+        .find({})
+        .then(persons => {
+            persons.forEach(person => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
+        })
+}
